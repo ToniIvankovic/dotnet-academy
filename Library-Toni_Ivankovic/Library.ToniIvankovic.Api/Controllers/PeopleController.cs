97563205a9b9b2e91d647331db2fb5d9e4137652
ToniIvankovic <toni.ivankovic@gmail.com>
2022-03-17 22:51:35 +0100

    First functioning controller 1.0

---
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Api/Controllers/PeopleController.cs
@@ -0,0 +1,40 @@
+﻿using Library.ToniIvankovic.Contracts.Dtos;
+using Library.ToniIvankovic.Contracts.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.ToniIvankovic.Api.Controllers
+{
+    [Route("host/api/people")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet("all")]
+        public IActionResult GetPersonsAll()
+        {
+            //TODO
+            return Ok();
+        }
+        [HttpGet]
+        public IActionResult GetPersonFromCity([FromQuery] string city)
+        {
+            //TODO
+            return Ok(city);
+        }
+        [HttpGet]
+        [Route("{personId:int}")]
+        public IActionResult GetPersonById([FromRoute] int personId)
+        {
+            //TODO
+            return Ok("a" + personId);
+        }
+
+        [HttpPost]
+        public IActionResult CreatePerson([FromBody] PersonDTO PersonDTO)
+        {
+            //TODO
+            return Ok(PersonDTO);
+        }
+    }
+
+}
