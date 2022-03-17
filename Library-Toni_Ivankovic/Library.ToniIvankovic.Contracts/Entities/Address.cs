644f894612690c58cbe5ee06f3dfff531dbc7f37
ToniIvankovic <toni.ivankovic@gmail.com>
2022-03-17 22:02:46 +0100

    Dodani Person i Address

---
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts/Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Library.ToniIvankovic.Contracts.Entities
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public Address(string street, string city, string country)
+        {
+            Street = street;
+            City = city;
+            Country = country;
+        }
+    }
+}
---
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts/Entities/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ToniIvankovic.Contracts.Entities
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address Address { get; set; }
+
+        public Person(int id, string firstName, string lastName, Address address)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+    }
+}
