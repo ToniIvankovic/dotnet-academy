9f32a3dc8f2de5fc220318e3745c853205898142
ToniIvankovic <toni.ivankovic@gmail.com>
2022-03-20 15:50:03 +0100

    Created default interfaces

---
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.ToniIvankovic.Contracts.Repositories
+{
+    public interface IRepository<TEntity>
+        where TEntity : class
+    {
+        Task<TEntity?> GetByIdAsync(int id);
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+
+        Task DeleteAsync(int id);
+
+        void Add(TEntity entity);
+
+        void Update(TEntity entity);
+    }
+}
---
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts/Repositories/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.ToniIvankovic.Contracts.Entities;
+
+namespace Library.ToniIvankovic.Contracts.Repositories
+{
+    internal interface IUnitOfWork
+    {
+        IRepository<Person> People { get; }
+
+        Task SaveChangesAsync();
+    }
+}
