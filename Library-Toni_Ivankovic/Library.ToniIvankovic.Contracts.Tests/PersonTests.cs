8016bbce7b4a894b4ad2c6dd6208a71a5a55a9df
ToniIvankovic <toni.ivankovic@gmail.com>
2022-04-05 21:10:45 +0200

    Added PersonTests

--- Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts.Tests/PersonTests.cs
+++ Library-Toni_Ivankovic/Library.ToniIvankovic.Contracts.Tests/PersonTests.cs
@@ -1,40 +1,78 @@
 using Library.ToniIvankovic.Contracts.Entities;
 using Library.ToniIvankovic.Contracts.Exceptions;
-using Library.ToniIvankovic.Contracts.Repositories;
-using Moq;
+using Library.ToniIvankovic.Data.Db.Configurations;
 using Xunit;
 
 namespace Library.ToniIvankovic.Contracts.Tests
 {
     public class PersonTests
     {
-        private Mock<IUnitOfWork> _uowMock;
         private Person _sut;
+        private readonly Book _book;
 
         public PersonTests()
         {
-            _uowMock = new Mock<IUnitOfWork>();
             _sut = new Person();
+            _book = new Book(1, "Bla", "Author1, Author2", Genre.FANTASY, 1);
         }
 
         [Fact]
         public void RentBook_WhenBookNotAvailable_ThenThrowException()
         {
-            Book b = new Book(1, "Bla", "Author1, Author2", Genre.FANTASY, 1);
-            //_uowMock.Setup(x => x.Books.GetByIdAsync(1))
-            //    .Returns(Task.FromResult(b));
-            new Person().RentBook(b);
-            Assert.Throws<BookNotAvailableException>(() => _sut.RentBook(b));
+            new Person().RentBook(_book);
+            Assert.Throws<BookNotAvailableException>(() => _sut.RentBook(_book));
         }
 
         [Fact]
         public void RentBook_WhenBookAvailable_DecreaseQuantity()
         {
-            Book b = new Book(1, "Bla", "Author1, Author2", Genre.FANTASY, 2);
-            Assert.Equal(2, b.Quantity);
-            new Person().RentBook(b);
-            Assert.Equal(1, b.Quantity);
+            Assert.Equal(1, _book.Quantity);
+            new Person().RentBook(_book);
+            Assert.Equal(0, _book.Quantity);
         }
 
+        [Fact]
+        public void RentBook_WhenTooManyRented_ThenThrowException()
+        {
+            IEnumerable<Book> books = BookMocks.Books;
+            books.Where(b => b.Id <= 4).ToList().ForEach(book => _sut.RentBook(book));
+            Assert.Throws<BookRentingException>(() => _sut.RentBook(books.Where(b => b.Id == 5).FirstOrDefault(_book)));
+        }
+
+        [Fact]
+        public void RentBook_WhenAlreadyRented_ThenThrowException()
+        {
+            _sut.RentBook(_book);
+            Assert.Throws<BookRentingException>(() => _sut.RentBook(_book));
+        }
+
+        [Fact]
+        public void ReturnBook_WhenNotRented_ThenThrowException()
+        {
+            Assert.Throws<BookRentingException>(() => _sut.ReturnBook(_book.Id));
+        }
+
+        [Fact]
+        public void ReturnBook_WhenRented_ThenIncreaseQuantity()
+        {
+            _sut.RentBook(_book);
+            _sut.ReturnBook(_book.Id);
+            Assert.Equal(1, _book.Quantity);
+        }
+
+        [Fact]
+        public void RentBook_WhenNotRented_ThenPutIntoList()
+        {
+            _sut.RentBook(_book);
+            Assert.Contains(_book, _sut.RentedBooks);
+        }
+
+        [Fact]
+        public void ReturnBook_WhenRented_ThenRemoveFromList()
+        {
+            _sut.RentBook(_book);
+            _sut.ReturnBook(_book.Id);
+            Assert.DoesNotContain(_book, _sut.RentedBooks);
+        }
     }
 }
